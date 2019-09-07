fd76e9ee40ece0d6faf6f619b2a830bc4494bed7
Raymond Ho <chunkiat82@gmail.com>
2019-09-07 18:47:53 +0800

    upload to S3

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-sudo chmod -R 777 `pwd`/data;
 docker run --env-file=`pwd`/phantomjs-rb.env -v `pwd`/data:/data -v `pwd`/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
