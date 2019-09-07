2580f05d98c139602ac63d425e139ddc335ca729
Raymond Ho <chunkiat82@gmail.com>
2019-09-07 16:50:45 +0800

    fixing

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-sudo chmod 777 `pwd`/data;
-sudo docker run --env-file=`pwd`/phantomjs-rb.env -v `pwd`/data:/data -v `pwd`/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
+docker run --env-file=`pwd`/phantomjs-rb.env -v `pwd`/data:/data -v `pwd`/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
