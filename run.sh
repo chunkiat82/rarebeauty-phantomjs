adf30eaadcda15d5e0ee977f90bf9993024e36d9
Raymond Ho <chunkiat82@gmail.com>
2019-09-07 16:57:02 +0800

    fixing

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 echo `pwd`
-echo '/data/jenkins/workspace/rarebeauty-daily-kibana'
-docker run --env-file=/data/jenkins/workspace/rarebeauty-daily-kibana/phantomjs-rb.env -v /data/jenkins/workspace/rarebeauty-daily-kibana/data:/data -v /data/jenkins/workspace/rarebeauty-daily-kibana/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
+echo '/var/jenkins_home/workspace/rarebeauty-daily-kibana'
+docker run --env-file=/var/jenkins_home/workspace/rarebeauty-daily-kibana/phantomjs-rb.env -v /var/jenkins_home/workspace/rarebeauty-daily-kibana/data:/data -v /var/jenkins_home/workspace/rarebeauty-daily-kibana/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
