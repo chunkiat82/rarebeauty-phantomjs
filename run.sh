b3c9051ff1b0d19e837aad716e8efc116fc18079
Raymond Ho <chunkiat82@gmail.com>
2019-09-07 16:39:41 +0800

    running in deamon mode

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run --env-file=`pwd`/phantomjs-rb.env -v `pwd`/data:/data -v `pwd`/phantom-rb.js:/phantom-rb.js -it wernight/phantomjs  phantomjs /phantom-rb.js
+docker run --env-file=`pwd`/phantomjs-rb.env -v `pwd`/data:/data -v `pwd`/phantom-rb.js:/phantom-rb.js -d wernight/phantomjs  phantomjs /phantom-rb.js
